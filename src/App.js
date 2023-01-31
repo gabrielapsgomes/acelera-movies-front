dbb5f1b8a1e9d0f25018bc0276cf3337c5703f0b
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-01-31 10:15:17 -0300

    Ajustes na tela de login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import FormularioLogin from './pages/login/login'
+import TelaHome from './pages/tela home/home'
+
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FormularioLogin />} exact />
+        <Route path="/home" element={<TelaHome />} exact />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/tela home/home.js
@@ -0,0 +1,5 @@
+const TelaHome = () => {
+  return true
+}
+
+export default TelaHome
