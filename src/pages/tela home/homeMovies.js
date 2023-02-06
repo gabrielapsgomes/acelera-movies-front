62e0d830bcd6f898bd1e1815edd989ba7903a3dc
gabrielapsgomes <gabrieladepaula8568@gmail.com>
2023-02-06 09:31:41 -0300

    Criacao tela home

--- src/pages/tela home/homeMovies.js
+++ src/pages/tela home/homeMovies.js
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './homeMovies.css'
 import TelaHome from './home'
+import { client } from '../../service/client'
 
 const HomeMovie = () => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    client.get('/movie').then(function (response) {
+      setMovies(response.data)
+    })
+  })
   return (
-  <div className='pageInteira'>
-     <div className='menu'>
-       <div className='hello'>
-        <h2>Hello, User!</h2>
-       </div>
-       < TelaHome />
-     </div>
-     <div className='main'>
-      <h1>All Movies</h1>
-      <div className='filmes'>
-        <div>
-
+    <div className="pageInteira">
+      <div className="menu">
+        <div className="hello">
+          <h2>Hello, User!</h2>
+        </div>
+        <TelaHome />
+      </div>
+      <div className="main">
+        <h1>All Movies</h1>
+        <div className="filmes">
+          <div>
+            {movies.map((movie, key) => (
+              <li key={key}>
+                <img src={movie.image} className='imagem'></img>
+                <h1 className="h1Titles">{movie.title}</h1>
+                <h1 className="h1Date">{movie.releaseDate}</h1>
+                <h2 className="h2Resume">Resume:</h2>
+                <h1 className="h1Resume">{movie.resume}</h1>
+                <h1 className="h1Note">{movie.note}</h1>
+              </li>
+            ))}
+          </div>
         </div>
       </div>
-     </div>
-  </div>
+    </div>
   )
 }
 
